32de8ebc048611fa0926674bbd7264937e9c78cc
hyper <hyper@Anduin>
2026-01-10 21:55:04 -0800

    fix: Update comment body to include link for creating new GitHub issue with reference to Reddit comment

--- snap_bot/comments/comment.py
+++ snap_bot/comments/comment.py
@@ -4,3 +4,7 @@ class Comment:
         self.author = author
         self.body = body
         self.url = url
+
+    def update_body_with_issue_link(self):
+        new_body = f"{self.body} [Create New Issue](https://github.com/alexloney/reddit_marvel_snap_card_bot/issues/new?body=[Original%20Comment]({self.url}))"
+        return Comment(self.id, self.author, new_body, self.url)
--- snap_bot/reddit_connect/reddit_connet.py
+++ snap_bot/reddit_connect/reddit_connet.py
@@ -11,18 +11,9 @@ class RedditConnect:
         self.seen_comments = []
 
     def get_my_username(self):
-        """
-        Get the username of the account running that is running. No need for an
-        extra query here to Reddit since we know the username from the config
-        file, so instead we will just use what we logged in with.
-        """
         return self.reddit.user.me().name
     
     def init_reddit_config(self, config_file: str):
-        """
-        Initialize the Reddit connection, use the details stored in the config
-        file to initiate a connection to Reddit
-        """
         with open(config_file) as f:
             config = json.load(f)
         
@@ -34,47 +25,33 @@ class RedditConnect:
             config['password'])
     
     def init_reddit_args(self, client_id: str, client_secret: str, user_agent: str, username: str, password: str):
-        """
-        Initialize the Reddit connection using the supplied details
-        """
         self.reddit = praw.Reddit(
-            client_id = client_id,
-            client_secret = client_secret,
-            username = username,
-            password = password,
-            user_agent = user_agent)
+            client_id=client_id,
+            client_secret=client_secret,
+            username=username,
+            password=password,
+            user_agent=user_agent)
 
         self.subreddit = self.reddit.subreddit(self.subreddit_name)
     
     @retry(times=3, interval=[1, 5, 10])
     def get_comments_shallow(self):
-        """
-        Get a list of comments, these may repeat as it pulls the last 100 comments
-        each time it runs, so when we query again, we may get comments we've
-        already seen. Because of that, we are using a `seen_comments` variable
-        to keep track of comments we have already seen to prevent sending those
-        back up to the caller
-        """
         comments = []
         count = 0
         for comment in self.subreddit.comments():
             count += 1
             if comment.id not in self.seen_comments:
                 self.seen_comments.append(comment.id)
-                comments.append(Comment(comment.id, comment.author.name, comment.body, 'https://reddit.com' + comment.permalink))
+                comment_url = 'https://reddit.com' + comment.permalink
+                github_issue_url = f"https://github.com/alexloney/reddit_marvel_snap_card_bot/issues/new?body=[Original Comment]({comment_url})"
+                comments.append(Comment(comment.id, comment.author.name, comment.body, github_issue_url))
         
-        # To prevent this list from simply growing to an unmanagable size, we
-        # will simply remove old entries when it has more than 150, since the
-        # query above only returns the past 100, this should be fine.
         while len(self.seen_comments) > 150:
             self.seen_comments.pop(0)
 
         return comments
     
     def get_comment_reply_author_names(self, id: str):
-        """
-        Fetch the names of the authors for a given comment.
-        """
         comment = self.reddit.comment(id)
         comment.refresh()
         authors = []
@@ -85,14 +62,8 @@ class RedditConnect:
 
     @retry(times=3, interval=[1, 5, 10])
     def add_reply(self, comment_id: str, reply_text: str):
-        """
-        Load a specific comment by ID and submit a reply to it
-        """
         comment = self.reddit.comment(comment_id)
         comment.reply(reply_text)
     
     def enable_readonly(self):
-        """
-        Enable read-only mode, useful for dry run
-        """
         self.reddit.read_only = True
